2fd35bc25d7128cfe0a907bb87e2941319f71f8e
Ubuntu <ubuntu@ip-172-31-40-72.ap-south-1.compute.internal>
2023-08-24 11:00:58 +0000

    v7

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,7 +3,7 @@
 # Get IDs of all running containers  
 container_ids=$(docker ps -q)
 
-# Loop through each container ID and remove it
+# LLoop through each container ID and remove it
 for container_id in $container_ids; do
     docker rm -f $container_id
 done
