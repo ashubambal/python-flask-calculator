08c38064e3572f12ac546de7f9d820dc7cadcc21
Ubuntu <ubuntu@ip-172-31-40-72.ap-south-1.compute.internal>
2023-08-25 04:04:45 +0000

    1.5

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub 
-docker pull softconsist/calculator-application:latest
+docker pull softconsist/dev-calculator-application:latest
 
 # Run the Docker image as a container y
-docker run -d -p 5000:5000 softconsist/calculator-application:latest
+docker run -d -p 5000:5000 softconsist/dev-calculator-application:latest
